96c6aa3d4fac2caca42cb90af2a654870e8ecaf3
Brandon Lee <brandonlee781@gmail.com>
2023-08-28 16:30:46 -0500

    Remove unused app service/controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { Module } from '@nestjs/common'
 import { MaintainenceModule } from './features/maintainence/maintainence.module'
 import { ConfigModule } from '@nestjs/config'
-import { AppLoggerMiddleware } from './middleware/app-logger/app-logger.middleware'
 import { DatabaseConfigModule } from './config/database/configuration.module'
 import { MysqlDatabaseModule } from './providers/database/MysqlDatabase.module'
-import { MaintainenceService } from './features/maintainence/maintainence.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
-import { LoggingPlugin } from './middleware/apollo-logger/apollo-logger.plugin'
 
 @Module({
   imports: [
@@ -23,8 +18,8 @@ import { LoggingPlugin } from './middleware/apollo-logger/apollo-logger.plugin'
     DatabaseConfigModule,
     MysqlDatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, MaintainenceService, LoggingPlugin],
+  controllers: [],
+  providers: [],
 })
 export class AppModule /*implements NestModule*/ {
   // configure(consumer: MiddlewareConsumer) {
